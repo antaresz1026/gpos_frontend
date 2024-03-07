61a9a8a441012973caa3846d9c42fbc7f8c6106d
锦阳 周 <467664111@qq.com>
2024-03-07 09:21:34 +0800

    完成了合成前的检查，下一步完成接收合成图片

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,13 +1,13 @@
 <template>
 	<u-tabbar
 		:value="page"
-		@change="(name) => PageChangeClick(name)"
+		@change="(name) => PageChange(name)"
 		:fixed="true"
 		:placeholder="false"
 		:safeAreaInsetBottom="true"
 	>
-		<u-tabbar-item name="home" text="首页" icon="home" @click="(name) => PageChangeClick(name)" ></u-tabbar-item>
-		<u-tabbar-item name="mine" text="我的" icon="account" @click="(name) => PageChangeClick(name)" ></u-tabbar-item>
+		<u-tabbar-item name="home" text="首页" icon="home" @click="(name) => PageChange(name)" ></u-tabbar-item>
+		<u-tabbar-item name="mine" text="我的" icon="account" @click="(name) => PageChange(name)" ></u-tabbar-item>
 	</u-tabbar>
 	
 	<view v-if="login == false && page == 'login'">
@@ -48,8 +48,8 @@
 				  <view class="card">
 					<text style="font-size: 4vh;color: white;margin-top: 2vh;margin-left: 2vw;">一键合影</text>
 					<text style="color: #e7e7e7;margin-top: 1vh;margin-left: 2vw;">快来一起“云游”四方</text>
-					<view style="width:36vw;height:5vh;margin-top: 6vh;margin-left: 2vw;">
-						<u-button shape="circle" text="开始使用" @click="PageChangeClick('GPOS')"></u-button>
+					<view style="width:36vw;height:5vh;margin-bottom: 3vh;margin-left: 2vw;">
+						<u-button shape="circle" text="开始使用" @click="PageChange('GPOS')"></u-button>
 					</view>
 				  </view>
 				  <view class="card">
@@ -71,22 +71,22 @@
 				</view>
 				<view class="nickname_display">
 					<u--text color="#000000" :text="nickname" :size="30" :bold="true" :lines="1" ></u--text>
-					<button style="position: absolute;width: 30vw;height: 4vh;margin-top: 2vh;border-radius: 10px;" v-if="login == false" @click="PageChangeClick('login')"></button>
+					<button style="position: absolute;width: 30vw;height: 4vh;margin-top: 2vh;border-radius: 10px;" v-if="login == false" @click="PageChange('login')"></button>
 					<text style="position: absolute;margin-top: 2vh;margin-left: 10vw;font-size: 20px;" v-if="login == false">登录</text>
 					<button style="position: absolute;width: 40vw;height: 4vh;margin-top: 2vh;border-radius: 10px;" v-if="login == true"></button>
 					<text style="position: absolute;margin-top: 2vh;width: 30vw;margin-left: 6vw;font-size: 20px;" v-if="login == true">编辑个人资料</text>
 				</view>
 			</view>
 			<u-cell-group :customStyle="cellgroups_style">
-				<u-cell name="graph" title="我的合影" isLink="true"  @click="(name) => PageChangeClick(name)"></u-cell>
-				<u-cell name="settings" title="设置" isLink="true" @click="(name) => PageChangeClick(name)"></u-cell>
+				<u-cell name="graph" title="我的合影" isLink=true @click="(name) => PageChange(name)"></u-cell>
+				<u-cell name="settings" title="设置" isLink=true @click="(name) => PageChange(name)"></u-cell>
 			</u-cell-group>
 		</view>
-	</view>\
+	</view>
 	<view v-if="page == 'GPOS'">
-		<u-navbar v-if="gpos_steps==0" :safeAreaInsetTop='false' left-text='返回' right-text="选择图片" title="选择背景图" @leftclick="GPOS_Steps('back')" @rightClick="ChooseImage('0')"></u-navbar>
-		<u-navbar v-if="gpos_steps==1" :safeAreaInsetTop='false' left-text='返回' right-text="选择图片" title="选择人像图" @leftclick="GPOS_Steps('back')" @rightClick="ChooseImage('1')"></u-navbar>
-		<u-navbar v-if="gpos_steps==2" :safeAreaInsetTop='false' left-text='返回' right-text="下一步" title="图像分割" @leftclick="GPOS_Steps('back')" @rightClick="GPOS_Steps('forward')"></u-navbar>
+		<u-navbar v-if="gpos_steps==0" :safeAreaInsetTop='false' left-text='返回' right-text="选择图片" title="选择背景图" @leftClick="GPOS_Steps('back')" @rightClick="ChooseImage('0')"></u-navbar>
+		<u-navbar v-if="gpos_steps==1" :safeAreaInsetTop='false' left-text='返回' right-text="选择图片" title="选择人像图" @leftClick="GPOS_Steps('back')" @rightClick="ChooseImage('1')"></u-navbar>
+		<u-navbar v-if="gpos_steps==2" :safeAreaInsetTop='false' left-text='返回' right-text="下一步" title="图像分割" @leftClick="GPOS_Steps('back')" @rightClick="GPOS_Steps('forward')"></u-navbar>
 		<view v-if="gpos_steps==0" class="page">
 			<view class="image_container">
 				<u--image :src="image_bgr" mode="aspectFit" shape="squre" width="80vw" height="60vh" radius="10"></u--image>
@@ -100,12 +100,28 @@
 				<u--image :src="image_src" mode="aspectFit" shape="squre" width="80vw" height="60vh" radius="10"></u--image>
 			</view>
 			<view class="next_step">
-				<button type="primary" hairline="true" @click="GPOS_Matting()">下一步</button>
+				<button type="primary" hairline="true" @click="GPOS_Upload()">下一步</button>
 			</view>
 		</view>
 		<view v-if="gpos_steps==2" class="page">
+			<u-overlay :show="matting_ready">
+				<view class="matting_check">
+					<view class="check_image_container">
+						<view class="check_image_preview">
+							<u--image :src="image_bgr" mode="aspectFit" shape="squre" width="42.5vw" height="30vh" radius="10"></u--image>
+						</view>
+						<view class="check_image_preview">
+							<u--image :src="image_src" mode="aspectFit" shape="squre" width="42.5vw" height="30vh" radius="10"></u--image>
+						</view>
+					</view>
+					<u--text type="warning" text="请确认要抠图的图片是否正确" align="center"></u--text>
+					<view class="check_confirm">
+						<button type="primary" hairline="true" @click="GPOS_Matting()">确认</button>
+					</view>
+				</view>
+			</u-overlay>
 			<view class="image_container">
-				<u--image :src="image_com" mode="aspectFit" shape="squre" width="80vw" height="60vh" radius="10"></u--image>
+				<u--image :src="default_avatar_url" mode="aspectFit" shape="squre" width="80vw" height="60vh" radius="10"></u--image>
 			</view>
 		</view>
 	</view>
@@ -121,18 +137,20 @@
 				avatar_url: default_avatar_url,
 				nickname: "默认用户",
 				nicknamepass: false,
-				page: "GPOS",
+				page: "login",
+				sessionID: "",
 				cellgroups_style: {
 					'position': 'relative',
 					'width': '100vw',
-					'margin-top': '10vh',
+					'margin-top': '20vh',
 					'background-color': '#ffffff'
 				},
 				gpos_steps: 0,
 				image_src: "",
 				image_bgr: "",
 				bg_chosen: "",
-				image_com: ""
+				image_com: "",
+				matting_ready: true,
 			} 
 		},
 		methods: {
@@ -160,7 +178,7 @@
 					this.login_access = true;
 				}
 			},
-			PageChangeClick(index) {
+			PageChange(index) {
 				this.page = index;
 			},
 			UserLogin() {
@@ -175,8 +193,10 @@
 									nickname: this.nickname
 								},
 								success: (res) => {
-									this.PageChangeClick("mine");
 									this.sessionID = res.data.sessionID;
+									this.login = true;
+									console.log(this.sessionID);
+									this.PageChange("home");
 								},
 								fail (error) {
 									wx.showToast({
@@ -201,7 +221,7 @@
 				}
 				if(this.gpos_steps == -1) {
 					this.gpos_steps = 0;
-					this.PageChangeClick('home');
+					this.PageChange('home');
 				}
 			},
 			ChooseImage(step) {
@@ -221,7 +241,7 @@
 					}
 				})
 			},
-			GPOS_Matting() {
+			GPOS_Upload() {
 				wx.uploadFile({
 					url: 'https://antaresz.cc:21026/GPOS/upload',
 					filePath: this.image_bgr,
@@ -240,10 +260,15 @@
 								sessionID: this.sessionID,
 							},
 							success: (res) => {
-								wx.showToast({
-									title: '图片上传成功',
-									icon: 'success'
-								});
+								console.log(res.statusCode+res.data);
+								if(res.statusCode == '200' ) {
+									wx.showToast({
+										title: '图片上传成功',
+										icon: 'success'
+									});
+									this.gpos_steps++;
+									this.matting_ready = true;
+								}
 							},
 							fail: (res) => {
 								wx.showToast({
@@ -259,6 +284,12 @@
 							icon: 'error'
 						})
 					}
+				})
+			},
+			GPOS_Matting() {
+				wx.request({
+					url: 'https://antaresz.cc:21026/GPOS/matting',
+					
 				})
 			}
 		}
@@ -377,5 +408,34 @@
 		width: 80vw;
 		margin-top: 5vh;
 		margin-left: 10vw;
+	}
+	.matting_check {
+		position: relative;
+		width: 90vw;
+		height: 50vh;
+		margin-left: 5vw;
+		margin-top: 10vh;
+		background-color: #f3f3f3;
+	}
+	.check_image_container {
+		position: relative;
+		width: 90vw;
+		height: 35vh;
+		display: flex;
+		flex-direction: row;
+	}
+	.check_image_preview {
+		width: 42.5vw;
+		height: 30vh;
+		margin-top: 2.5vh;
+		margin-left: 1.25vw;
+		margin-right: 1.25vw;
+		margin-bottom: 2.5vh;
+	}
+	.check_confirm {
+		width: 60vw;
+		height: 10vh;
+		margin-top: 2vh;
+		margin-left: 15vw;
 	}
 </style>
